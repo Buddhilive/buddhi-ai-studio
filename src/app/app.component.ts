08e13d65a108057418bf545797fac2a7d237f958
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-04-29 10:37:44 +0530

    Clean project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,27 @@ import { FooterComponent } from "./layout/footer/footer.component";
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavbarComponent, SidebarComponent, FooterComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `<!--begin::App Wrapper-->
+  <div class="app-wrapper">
+    <!--begin::Header-->
+    <app-navbar></app-navbar>
+    <!--end::Header-->
+    <!--begin::Sidebar-->
+    <app-sidebar class="app-sidebar"></app-sidebar>
+    <!--end::Sidebar-->
+    <!--begin::App Main-->
+    <main class="app-main">
+      <router-outlet></router-outlet>
+    </main>
+    <!--end::App Main-->
+    <!--begin::Footer-->
+    <footer class="app-footer">
+      <app-footer></app-footer>
+    </footer>
+    <!--end::Footer-->
+  </div>
+  <!--end::App Wrapper-->
+  `,
 })
 export class AppComponent {
 }
--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   imports: [],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  template: `<!--begin::To the end-->
+  <div class="float-end d-none d-sm-inline">Anything you want</div>
+  <!--end::To the end-->
+  <!--begin::Copyright-->
+  <strong>
+    Copyright &copy; 2015 - 2025&nbsp;
+    <a href="https://buddhilive.com" class="text-decoration-none">Buddhilive</a
+    >.
+  </strong>
+  All rights reserved.
+  <!--end::Copyright-->`,
 })
 export class FooterComponent {
 
