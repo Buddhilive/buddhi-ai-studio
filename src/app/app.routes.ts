16008dac5e1094d8b7d9720ccb268bc50758e039
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-04-29 06:30:35 +0530

    Update navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SummarizationComponent } from './pages/summarization/summarization.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: DashboardComponent
+    },
+    {
+        path: 'summarizer',
+        component: SummarizationComponent
     },
     {
         path: '**',
