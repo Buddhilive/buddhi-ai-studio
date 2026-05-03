c98c490e948cebfa2de0c3af54b87dfc9faa077c
Buddhi Kavindra <visitbudkavin@gmail.com>
2026-05-03 11:25:27 +0530

    Remove old implementation

--- core/database/deps.py
+++
@@ -1,8 +0,0 @@
-from fastapi import Depends
-from sqlalchemy.orm import Session
-from core.database.engine import get_session
-
-
-def get_db(session: Session = Depends(get_session)) -> Session:
-    """FastAPI dependency to get a database session."""
-    return session
