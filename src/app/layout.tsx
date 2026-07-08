ee0c30f7d43d585abac6d57b46ef29e5ab508c9b
Buddhi Kavindra <visitbudkavin@gmail.com>
2026-07-08 15:05:28 +0530

    Add AI Elements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { AppSidebar } from "@/components/app-sidebar"
+import { AppSidebar } from "@/components/app-sidebar";
 import {
   Breadcrumb,
   BreadcrumbItem,
